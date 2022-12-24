9046b544f6400759bcbe4d0d437139afb0ca83a2
bayu <bayusetiawanipb@gmail.com>
2022-12-24 10:20:33 +0700

    setup architecture and dependency

---
+++ src/components/repo-card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RepoCard = () => {
+  return (
+    <div>
+      Repo Card
+    </div>
+  );
+};
+
+export default RepoCard;
---
+++ src/services/github.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GithubProvider = () => {
+  return (
+    <div>
+      GithubProvider
+    </div>
+  );
+};
+
+export default GithubProvider;
