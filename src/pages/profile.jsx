7b0fa0632c19830d98d2f7d7ab5ac729a7b6cf4b
bayu <bayusetiawanipb@gmail.com>
2022-12-24 11:35:35 +0700

    create service: user profile, public repo, all repo with auth

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useGithub } from '../services/github';
 
 const Profile = () => {
+  const {
+    user,
+    getGithubUser,
+    repos,
+    getPublicRepository,
+    allRepos,
+    getAllRepository,
+  } = useGithub()
+
+  useEffect(() => {
+    getGithubUser('bayusetiawan99')
+    getPublicRepository('bayusetiawan99')
+    getAllRepository()
+  }, [])
+
   return (
     <div>
       Profile
